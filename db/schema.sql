42aabba7d125ae31957b1f510510c4e0220e4b95
Austin WIley <wileybeer@gmail.com>
2019-02-08 22:05:37 -0700

    commit

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,10 @@
 CREATE DATABASE burger_sequalize_d;
 USE burger_sequalize_d;
+
+CREATE TABLE burgers (
+  id Int( 11 ) AUTO_INCREMENT,
+  burger_name VARCHAR( 255) NOT NULL,
+  devoured BOOLEAN NOT NULL,
+  createdAt TIMESTAMP NOT NULL,
+  PRIMARY KEY ( id ) 
+);
