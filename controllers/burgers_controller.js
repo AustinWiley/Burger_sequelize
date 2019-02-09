670b05a707a116d3bc3bd208d7a119a82d6c6d67
Austin WIley <wileybeer@gmail.com>
2019-02-08 20:54:01 -0700

    final cleanup

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -33,55 +33,18 @@ router.post("/", function (req, res) {
 
 router.put("/burger/:burger", function (req, res) {
   console.log(req.body)
-  const newData = {  
+  const newData = {
     devoured: 1
   };
-  
-  burger.update(newData, {where: { burger_name: req.body.devoured } })  
-  .then(updatedBurger => {
-    console.log(updatedBurger)
-  })
+  burger.update(newData, {
+      where: {
+        burger_name: req.body.devoured
+      }
+    })
+    .then(data => {
+      console.log(data);
+      res.json(data);
+    })
 });
 
-
-
-
-
-
-
-
-
-
-
-
-// router.get("/", function(req, res) {
-//   burger.all(function(data) {
-//     var hbsObject = {
-//       burgers: data
-//     };
-//     console.log(hbsObject);
-//     res.render("index", hbsObject);
-//   });
-// });
-
-// router.post("/", function(req, res) {
-//   burger.create(req.body.name,  function(result) {
-//     console.log("this is the result:"+result)
-//     res.json(result)
-//   });
-// });
-
-// router.put("/burger/:burger", function (req, res) {
-//   burger.update(req.body.devoured, function (result) {
-//     // if (result.changedRows == 0) {
-//     //   // If no rows were changed, then the burger must not exist, so 404
-//     //   return res.status(404).end();
-//     // } else {
-//     //   res.status(200).end();
-//     // }
-//     res.json(result)
-//   });
-// });
-
-// Export routes for server.js to use.
 module.exports = router;
