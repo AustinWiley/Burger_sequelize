8eb3994b7fb294a0aea57b3d421ac96d05a1cb70
Austin WIley <wileybeer@gmail.com>
2019-02-08 22:18:53 -0700

    commit

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ if (process.env.JAWSDB_URL) {
 
   // var sequelize = new Sequelize(process.env.JAWSDB_URL);
   var sequelize = new Sequelize('iz6gd16bn9rgtl97', '	i5nt37zz6f4tch7y', 'p5glsnqv1cexpkzb', {
-    host: 'jsftj8ez0cevjz8v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	',
+    host: 'jsftj8ez0cevjz8v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     port: 3306,
     dialect: 'mysql',
     operatorsAliases: false,
